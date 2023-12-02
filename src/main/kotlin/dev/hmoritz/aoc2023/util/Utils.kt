3bb19ab18e5a7644993090acfacc91bb50a3cf85
Hayden Moritz <moritz.hayden@gmail.com>
2023-12-01 23:27:33 -0600

    Refactor wordToDigit

--- src/main/kotlin/dev/hmoritz/aoc2023/util/Utils.kt
+++ src/main/kotlin/dev/hmoritz/aoc2023/util/Utils.kt
@@ -28,16 +28,16 @@ object Utils {
         if (word.toDoubleOrNull() != null) return word.toInt()
 
         return when (word) {
-            "nine" -> 9
-            "eight" -> 8
-            "seven" -> 7
-            "six" -> 6
-            "five" -> 5
-            "four" -> 4
-            "three" -> 3
-            "two" -> 2
-            "one" -> 1
             "zero" -> 0
+            "one" -> 1
+            "two" -> 2
+            "three" -> 3
+            "four" -> 4
+            "five" -> 5
+            "six" -> 6
+            "seven" -> 7
+            "eight" -> 8
+            "nine" -> 9
             else -> throw IllegalArgumentException("Invalid word: $word")
         }
     }
