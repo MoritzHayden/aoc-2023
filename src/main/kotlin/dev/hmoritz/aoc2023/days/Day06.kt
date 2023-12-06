3bb4509407a32416006afa29f98275d36ad05c4f
Hayden Moritz <moritz.hayden@gmail.com>
2023-12-05 23:34:18 -0600

    Finish day 6 part 2

--- src/main/kotlin/dev/hmoritz/aoc2023/days/Day06.kt
+++ src/main/kotlin/dev/hmoritz/aoc2023/days/Day06.kt
@@ -35,8 +35,14 @@ class Day06() : Day {
     }
 
     private fun solvePart2(): String {
-        // STUB
-        return ""
+        val race = getRacePart2()
+        var waysToWin = 0
+        for (buttonHoldTime in 0..race.first) {
+            val remainingTime = race.first - buttonHoldTime
+            val distance = remainingTime * buttonHoldTime
+            if (distance > race.second) waysToWin++
+        }
+        return waysToWin.toString()
     }
 
     // Returns a list of pairs in the form (time, distance)
@@ -44,5 +50,12 @@ class Day06() : Day {
         val times = input[0].replace("Time:", "").trim().split("\\s+".toRegex()).map { it.toInt() }
         val distances = input[1].replace("Distance:", "").trim().split("\\s+".toRegex()).map { it.toInt() }
         return times.zip(distances)
+    }
+
+    // Returns a pair in the form (time, distance)
+    private fun getRacePart2(): Pair<Long, Long> {
+        val time = input[0].replace("Time:", "").trim().replace("\\s+".toRegex(), "").toLong()
+        val distance = input[1].replace("Distance:", "").trim().replace("\\s+".toRegex(), "").toLong()
+        return Pair(time, distance)
     }
 }
