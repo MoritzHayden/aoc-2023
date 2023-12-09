678b44085eea9a2fb355a6166b551749d47de6ec
Hayden Moritz <moritz.hayden@gmail.com>
2023-12-08 23:41:17 -0600

    wip: day 8 part 1

--- src/main/kotlin/dev/hmoritz/aoc2023/days/Day08.kt
+++ src/main/kotlin/dev/hmoritz/aoc2023/days/Day08.kt
@@ -5,6 +5,7 @@
 package dev.hmoritz.aoc2023.days
 
 import dev.hmoritz.aoc2023.models.Day
+import dev.hmoritz.aoc2023.util.BinaryTreeNode
 import dev.hmoritz.aoc2023.util.Constants.filenames
 import dev.hmoritz.aoc2023.util.Utils.readFile
 import dev.hmoritz.aoc2023.util.Utils.writeSolutionsToFile
@@ -20,12 +21,50 @@ class Day08() : Day {
     }
 
     private fun solvePart1(): String {
-        // STUB
-        return ""
+        // TODO: This works on the test input, but hangs on the real input.
+        //  May need different data structure or remove cyclical references.
+        val instructions = parseInstructions()
+        val root = parseTree()
+        var currentNodeValue = "AAA"
+        var steps = 0
+        println()
+        while (currentNodeValue != "ZZZ") {
+            steps++
+            val instruction = instructions[(steps - 1) % instructions.size]
+            print("Step $steps: Moving $instruction from $currentNodeValue to ")
+            when (instruction) {
+                'L' -> currentNodeValue = root.find(currentNodeValue)?.getLeft()?.getValue() ?: currentNodeValue
+                'R' -> currentNodeValue = root.find(currentNodeValue)?.getRight()?.getValue() ?: currentNodeValue
+            }
+            println(currentNodeValue)
+        }
+        return steps.toString()
     }
 
     private fun solvePart2(): String {
         // STUB
         return ""
+    }
+
+    private fun parseInstructions(): CharArray {
+        return input[0].toCharArray()
+    }
+
+    private fun parseTree(): BinaryTreeNode<String> {
+        val tree = BinaryTreeNode("AAA")
+        for (i in 2..<input.size) {
+            val splitLine = input[i].split(" = ")
+            val root = splitLine[0]
+            val children = splitLine[1]
+                .replace("(", "")
+                .replace(")", "")
+                .replace(",", "")
+                .split(" ")
+            val left = children[0]
+            val right = children[1]
+            if (root != left) tree.find(root)?.addLeft(left)
+            if (root != right) tree.find(root)?.addRight(right)
+        }
+        return tree
     }
 }
