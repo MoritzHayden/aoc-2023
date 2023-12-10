526952f899752c98bf33db705818d8c53775935f
Hayden Moritz <moritz.hayden@gmail.com>
2023-12-10 17:17:10 -0600

    Add findLCM methods

--- src/main/kotlin/dev/hmoritz/aoc2023/util/Utils.kt
+++ src/main/kotlin/dev/hmoritz/aoc2023/util/Utils.kt
@@ -52,6 +52,28 @@ object Utils {
 
     fun isSymbol(char: Char?) = char in symbols
 
+    fun findLCM(a: Long, b: Long): Long {
+        val larger = if (a > b) a else b
+        val maxLcm = a * b
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm % a == 0L && lcm % b == 0L) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
+
+    fun findLCM(numbers: List<Long>): Long {
+        var result = numbers[0]
+        for (i in 1 until numbers.size) {
+            result = findLCM(result, numbers[i])
+        }
+        return result
+    }
+
+
     // Convert the given string to a resource URI
     private fun String.toURI(): URI =
         Utils.javaClass.classLoader.getResource(this)?.toURI()
